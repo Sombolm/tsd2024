92c44cf255ecdfa58df985972faa00d255d40824
Miłosz Sobol <miloszsobolsci@gmail.com>
2026-03-17 19:56:34 +0100

    TASK-1 Fixed starting date for collecting data

--- 03-LINQ/GoldSavings.App/Program.cs
+++ 03-LINQ/GoldSavings.App/Program.cs
@@ -12,9 +12,8 @@ class Program
     {
         Console.WriteLine("Hello, Gold Investor!");
 
-        // Step 1: Get gold prices
         GoldDataService dataService = new GoldDataService();
-        DateTime startDate = new DateTime(2024,09,18);
+        DateTime startDate = new DateTime(2019, 1, 1);
         DateTime endDate = DateTime.Now;
         List<GoldPrice> goldPrices = dataService.GetGoldPrices(startDate, endDate).GetAwaiter().GetResult();
 
@@ -26,9 +25,7 @@ class Program
 
         Console.WriteLine($"Retrieved {goldPrices.Count} records. Ready for analysis.");
 
-        // Step 2: Perform analysis
-        GoldAnalysisService analysisService = new GoldAnalysisService(goldPrices);
-        var avgPrice = analysisService.GetAveragePrice();
+        AnswerAllQuestions(goldPrices);
 
         //Task 3 and 4:
         string xmlFilePath = "goldPrices.xml";
