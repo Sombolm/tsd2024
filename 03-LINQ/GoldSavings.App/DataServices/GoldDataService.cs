7415c52c7edb9f65fdcaf57c55f1b6280a735034
milosz.sobol <milosz.sobol@beone.pl>
2026-03-17 18:47:38 +0100

    Finished TASK-1 A-D

--- 03-LINQ/GoldSavings.App/DataServices/GoldDataService.cs
+++ 03-LINQ/GoldSavings.App/DataServices/GoldDataService.cs
@@ -17,8 +17,28 @@ namespace GoldSavings.App.Services
 
         public async Task<List<GoldPrice>> GetGoldPrices(DateTime startDate, DateTime endDate)
         {
-            var prices = await _goldClient.GetGoldPrices(startDate, endDate);
-            return prices ?? new List<GoldPrice>();  // Prevent null values
+            List<GoldPrice> allPrices = new List<GoldPrice>();
+            DateTime currentStart = startDate;
+
+            while (currentStart <= endDate)
+            {
+                DateTime currentEnd = currentStart.AddDays(92); 
+                if (currentEnd > endDate)
+                {
+                    currentEnd = endDate;
+                }
+
+                var pricesChunk = await _goldClient.GetGoldPrices(currentStart, currentEnd);
+                
+                if (pricesChunk != null)
+                {
+                    allPrices.AddRange(pricesChunk);
+                }
+
+                currentStart = currentEnd.AddDays(1);
+            }
+
+            return allPrices;
         }
     }
 }
